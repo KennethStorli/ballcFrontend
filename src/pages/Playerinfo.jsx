9903e13163771e45b1d4f90567b84855ff8758ed
Biancaeilertsen <biancaeilertsen@gmail.com>
2018-11-07 13:31:24 +0100

    work work work work work work

--- src/pages/Playerinfo.jsx
+++ src/pages/Playerinfo.jsx
@@ -1,5 +1,5 @@
 import React, { Component} from 'react';
-import {Col} from 'react-bootstrap'
+import {Col, ListGroup, ListGroupItem} from 'react-bootstrap'
 import './Teams.css'
 
 
@@ -11,9 +11,8 @@ export default class Coach extends Component{
         players:[],
         allplayers: this.props.person,
       };
-
     }
-    componentWillReceiveProps(nextprop) {
+    componentDidMount() {
       fetch(`https://ballc-backend-api.herokuapp.com/players`)
       .then(result => result.json())
       .then(players => this.setState({players}))
@@ -22,13 +21,26 @@ export default class Coach extends Component{
     render() {
       let filteredPlayers = this.state.players.filter(
         (players) => {
-          return this.state.allplayers.indexOf(this.state.players)
+          return this.state.allplayers.indexOf(this.state.players) !== -1
         }
       )
 
       return (
           <div>
-            {filteredPlayers.map(players =>
+            <ListGroup>
+              <div>
+                {this.state.players.map(name =>
+                  <ListGroupItem
+                    className="listingplayer"
+                    key={name.person_id}>
+                    {name.first_name} {name.last_name}
+                  </ListGroupItem>)}
+              </div>
+            </ListGroup>
+
+            {      console.log(this.state.players)
+            }
+            {this.state.players.map(players =>
               <Col xs={12} sm={3} key={players.player_id}>
                 <h2>{this.state.players.first_name}</h2>
               </Col>
--- src/pages/Teaminfo.jsx
+++ src/pages/Teaminfo.jsx
@@ -6,13 +6,16 @@ import Coach from './Coach';
 import Playerinfo from './Playerinfo'
 import './Teams.css'
 
-import { Row, Grid   } from 'react-bootstrap'
+import { Row, Grid, Col, ListGroup, ListGroupItem   } from 'react-bootstrap'
 
 export default class Teaminfo extends Component{
     constructor(props) {
       super(props);
       this.state = {
+        players:[],
+        playersInTeam:[],
         team:[],
+        teams:[],
         teamID: props.location.state.id,
       };
       console.log('Teams is:', this.state.teamID)
@@ -22,12 +25,21 @@ export default class Teaminfo extends Component{
     componentDidMount() {
       fetch(`https://ballc-frontend-be.herokuapp.com/team/${this.state.teamID}`)
       .then(result => result.json())
-      .then(team => this.setState({team}))
+      .then(team => this.setState({team,
+      playersInTeam: team.players}))
+
+      fetch(`https://ballc-backend-api.herokuapp.com/players/team/${this.state.teamID}`)
+      .then(result => result.json())
+      .then(players => this.setState({players}))
     }
 
 
     render() {
-
+      let filteredPlayers = this.state.players.filter(
+        (players) => {
+          return this.state.team
+        }
+      )
 
       return (
         <div>
@@ -40,10 +52,28 @@ export default class Teaminfo extends Component{
               <Location location={this.state.team.location}/>
               <Owner owner={this.state.team.owner}/>
               <Coach coach={this.state.team.coach}/>
+              <br/>
+              <br/>
+
+              <hr/>
               <h1 className="playerinfo">Players</h1>
               <br/>
               <br/>
-              <Playerinfo allplayers={this.state.team.players}/>
+
+
+              {this.state.players.map(players =>
+                <Col xs={12} sm={4} key={players.person_id}>
+                  <div className="playerseparator">
+
+                    <h3 className="greytext">{players.normal_position}</h3>
+                    <hr/>
+                    <h3>{players.first_name} {players.last_name}</h3>
+                    <p>{players.date_of_birth}</p>
+                  </div>
+
+                  </Col>
+                  )}
+
             </Row>
           </Grid>
           </div>
