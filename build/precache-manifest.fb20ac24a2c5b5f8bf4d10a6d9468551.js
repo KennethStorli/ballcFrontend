ef5ba353eebb61a4b878127d499ecf2f58245795
Kenneth Storli <kenneth.storli@gmail.com>
2018-11-08 14:14:29 +0100

    First failing build

---
+++ build/precache-manifest.fb20ac24a2c5b5f8bf4d10a6d9468551.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "42cb83e7a5fa0a1641ec",
+    "url": "/static/js/main.42cb83e7.chunk.js"
+  },
+  {
+    "revision": "bd699f8cf16421266ff4",
+    "url": "/static/js/1.bd699f8c.chunk.js"
+  },
+  {
+    "revision": "42cb83e7a5fa0a1641ec",
+    "url": "/static/css/main.8bada834.chunk.css"
+  },
+  {
+    "revision": "bd699f8cf16421266ff4",
+    "url": "/static/css/1.26dda6dd.chunk.css"
+  },
+  {
+    "revision": "60d605f2b7cf6db6cbf065a9fb4fc1c3",
+    "url": "/index.html"
+  }
+];
